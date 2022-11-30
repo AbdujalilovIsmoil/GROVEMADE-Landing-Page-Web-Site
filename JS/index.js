fd86741f7dddf096c5de160df236c6cd85e92397
= <ismoilbek20050421@gmail.com>
2022-11-30 23:39:02 +0500

    The End GROVEMADE WEB SITE

--- JS/index.js
+++ JS/index.js
@@ -1,14 +1,20 @@
-$(".fa-bars").addEventListener("click", () => {
-  $(".menu").classList.add("active");
-});
-$(".fa-times").addEventListener("click", () => {
-  $(".menu").classList.remove("active");
-});
-
-for (let i = 0; i < $$(".menu__link").length; i++) {
-  $$(".menu__link")[i].addEventListener("click", () => {
+this.addEventListener("DOMContentLoaded", () => {
+  // ****NAVBAR CODES START****
+  $(".fa-bars").addEventListener("click", () => {
+    $(".menu").classList.add("active");
+  });
+  $(".fa-times").addEventListener("click", () => {
     $(".menu").classList.remove("active");
   });
-}
-AOS.init();
 
+  for (let i = 0; i < $$(".menu__link").length; i++) {
+    $$(".menu__link")[i].addEventListener("click", () => {
+      $(".menu").classList.remove("active");
+    });
+  }
+
+  // ****NAVBAR CODES END****
+
+  // ****AOS ANIMATION LIBRARY****
+  AOS.init();
+});
